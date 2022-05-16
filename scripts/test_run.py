38761790968ff047483e6ef35e7a48fa58866348
Gregor Monson <gregor.monson@fanduel.com>
2022-05-16 19:51:48 +0100

    Refactored for airflow

---
+++ scripts/test_run.py
@@ -0,0 +1,3 @@
+from scripts.rightmove_scrape import get_for_sale_properties
+
+get_for_sale_properties('5E475')
--- test_run.py
+++
@@ -1,3 +0,0 @@
-from rightmove_scrape import get_for_sale_properties
-
-get_for_sale_properties('5E475')
