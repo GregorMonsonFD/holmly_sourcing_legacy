1f26f1c126d9c0e74250a1ecfa965488ce8e4b41
Gregor Monson <gregor.monson@fanduel.com>
2022-05-17 18:53:20 +0100

    Added test dag for edinburgh import"

---
+++ dags/import_edinburgh_rightmove.py
@@ -0,0 +1,24 @@
+rom airflow.models import DAG
+from airflow.operators.python_operator import PythonOperator
+from airflow.utils.dates import days_ago
+from scripts.rightmove_scrape import get_for_sale_properties
+
+#https://www.youtube.com/watch?v=IsWfoXY_Duk
+
+args = {
+    'owner': 'Gregor Monson',
+    'start_date': days_ago(1) # make start date in the past
+}
+
+dag = DAG(
+    dag_id='rightmove-scrape-edinburgh',
+    default_args=args,
+    schedule_interval='0 0 * * *' # make this workflow happen every day
+)
+
+with dag:
+    rightmove_edinburgh_to_csv = PythonOperator(
+        task_id='rightmove_edinburgh_to_csv',
+        python_callable=get_for_sale_properties('5E475'),
+        # provide_context=True
+    )
