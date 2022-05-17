5a2b2a26dfc24759e9c428e8e624087f0bc3ddea
Gregor Monson <gregor.monson@fanduel.com>
2022-05-18 00:58:06 +0100

    Added SFTP operation

--- dags/import_edinburgh_rightmove.py
+++ dags/import_edinburgh_rightmove.py
@@ -1,5 +1,6 @@
 from airflow.models import DAG
 from airflow.operators.python_operator import PythonOperator
+from airflow.providers.sftp.operators.sftp import SFTPOperator
 from airflow.utils.dates import days_ago
 from scripts.rightmove_scrape import get_for_sale_properties
 import datetime
@@ -8,11 +9,13 @@ import datetime
 
 args = {
     'owner': 'Gregor Monson',
-    'start_date': days_ago(1) # make start date in the past
+    'start_date': days_ago(0), # make start date in the past
+    'yesterday': days_ago(1),
+    'borough_code': '5E475'
 }
 
 dag = DAG(
-    dag_id='rightmove-scrape-edinburgh',
+    dag_id='rightmove-edinburgh',
     default_args=args,
     schedule_interval='0 0 * * *', # make this workflow happen every day
 )
@@ -23,6 +26,18 @@ with dag:
         provide_context=True,
         python_callable=get_for_sale_properties,
         execution_timeout=datetime.timedelta(seconds=300),
-        op_kwargs={'borough': '5E475'},
+        op_kwargs={'borough': '{{ borough_code }}'},
         retries=2,
     )
+
+    ftp_upload_edinburgh_to_db = SFTPOperator(
+        task_id="sftp_pi_to_warehouse",
+        ssh_conn_id="ssh_default",
+        local_filepath="/home/eggzo/tmp_data/sales_data_{{ borough_code }}_{{ start_date }}.csv",
+        remote_filepath="/var/lib/mysql-files/sales_data_{{ borough_code }}_{{ start_date }}.csv",
+        operation="put",
+        create_intermediate_dirs=True,
+        dag=dag
+    )
+
+    rightmove_edinburgh_to_csv >> ftp_upload_edinburgh_to_db
