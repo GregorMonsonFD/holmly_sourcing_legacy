b08d9eac7709a6e1253df2798562f35ed5b60c15
Gregor Monson <gregor.monson@fanduel.com>
2022-05-17 19:29:58 +0100

    arguments in wrong place oops

--- dags/import_edinburgh_rightmove.py
+++ dags/import_edinburgh_rightmove.py
@@ -15,15 +15,14 @@ dag = DAG(
     dag_id='rightmove-scrape-edinburgh',
     default_args=args,
     schedule_interval='0 0 * * *', # make this workflow happen every day
-    execution_timeout=datetime.timedelta(seconds=300),
-    timeout=3600,
-    retries=2,
-    mode="reschedule",
 )
 
 with dag:
     rightmove_edinburgh_to_csv = PythonOperator(
         task_id='rightmove_edinburgh_to_csv',
         python_callable=get_for_sale_properties('5E475'),
-        # provide_context=True
+        execution_timeout=datetime.timedelta(seconds=300),
+        timeout=3600,
+        retries=2,
+        mode="reschedule",
     )
