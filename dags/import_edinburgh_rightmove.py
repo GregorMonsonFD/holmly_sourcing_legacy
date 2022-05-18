4471d99eff2df0de924b78c09888058458b7f837
Gregor Monson <gregor.monson@fanduel.com>
2022-05-18 01:22:37 +0100

    Added airflow variables

--- dags/import_edinburgh_rightmove.py
+++ dags/import_edinburgh_rightmove.py
@@ -5,13 +5,11 @@ from airflow.utils.dates import days_ago
 from scripts.rightmove_scrape import get_for_sale_properties
 import datetime
 
-#https://www.youtube.com/watch?v=IsWfoXY_Duk
-
 args = {
     'owner': 'Gregor Monson',
-    'start_date': days_ago(0), # make start date in the past
-    'yesterday': days_ago(1),
-    'borough_code': '5E475'
+    'start_date': days_ago(0),
+    'yesterday': days_ago(1), # make start date in the past
+    'borough_code': '{{ var.value.<edinburgh_id> }}'
 }
 
 dag = DAG(
@@ -28,13 +26,14 @@ with dag:
         execution_timeout=datetime.timedelta(seconds=300),
         op_kwargs={'borough': '{{ borough_code }}'},
         retries=2,
+        dag=dag,
     )
 
     ftp_upload_edinburgh_to_db = SFTPOperator(
         task_id="sftp_pi_to_warehouse",
         ssh_conn_id="ssh_default",
-        local_filepath="/home/eggzo/tmp_data/sales_data_{{ borough_code }}_{{ start_date }}.csv",
-        remote_filepath="/var/lib/mysql-files/sales_data_{{ borough_code }}_{{ start_date }}.csv",
+        local_filepath="/home/eggzo/tmp_data/sales_data_{{ var.value.<edinburgh_id> }}_{{ ds }}.csv",
+        remote_filepath="/var/lib/mysql-files/sales_data_{{ var.value.<edinburgh_id> }}_{{ ds }}.csv",
         operation="put",
         create_intermediate_dirs=True,
         dag=dag
