057ea244b1089164031cd8349e208e5a0667bea7
Gregor Monson <gregor.monson@fanduel.com>
2022-05-21 20:29:54 +0100

    Fixed sql order

--- scripts/sql/edi/csv_to_landing_area_edi.sql
+++ scripts/sql/edi/csv_to_landing_area_edi.sql
@@ -1,5 +1,3 @@
-DROP TABLE IF EXISTS landing.edinburgh{{ ds_nodash }};
-
 LOAD DATA INFILE '/var/lib/mysql-files/sales_data_{{ var.value.edinburgh_id }}_{{ ds }}.csv'
 INTO TABLE landing.edinburgh{{ ds_nodash }}
 FIELDS TERMINATED BY ','
