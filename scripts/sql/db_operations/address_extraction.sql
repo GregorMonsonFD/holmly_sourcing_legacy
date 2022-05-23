926a3bf0f3d4feef562447461a49542444c58529
Gregor Monson <gregor.monson@fanduel.com>
2022-05-23 21:23:29 +0100

    Improving postcode extraction

--- scripts/sql/db_operations/address_extraction.sql
+++ scripts/sql/db_operations/address_extraction.sql
@@ -1,23 +1,24 @@
-DROP TABLE IF EXISTS staging.address_extraction_{{ params.region_name }};
-CREATE TEMPORARY TABLE staging.address_extraction_{{ params.region_name }}
+DROP TABLE IF EXISTS staging.address_extraction_{{ params.region_name }}_stage_1;
+CREATE TEMPORARY TABLE staging.address_extraction_{{ params.region_name }}_stage_1
 SELECT
     ID AS _ID,
     address AS _address,
     reverse(SUBSTRING_INDEX(reverse(address), reverse('{{ params.postcode_prefix }}'), 1)) AS _postcode
 FROM landing.{{ params.region_name }}{{ ds_nodash }};
 
-SELECT count(*) FROM staging.address_extraction_{{ params.region_name }}
-WHERE LENGTH(_postcode) <6;
+CREATE TEMPORARY TABLE staging.address_extraction_{{ params.region_name }}_stage_2
+SELECT _ID, _postcode FROM staging.address_extraction_{{ params.region_name }}_stage_1
+WHERE _postcode REGEXP '[0-9]';
 
 UPDATE
     staging.{{ params.region_name }} STG,
-    staging.address_extraction_{{ params.region_name }} TMP
+    staging.address_extraction_{{ params.region_name }}_stage_2 TMP
 SET
     full_address = _address,
     postcode =
     IF
     (
-        LENGTH(_postcode) < 10 AND _postcode LIKE '%[0-9]%',
+        LENGTH(_postcode) < 10,
         CONCAT(REPLACE('{{ params.postcode_prefix }}', ' ', ''), _postcode),
         null
     )
