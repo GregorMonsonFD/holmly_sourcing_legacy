c32bd2ce5da17a53e739b3357999c0edc1398c24
Gregor Monson <gregor.monson@fanduel.com>
2022-05-23 23:16:01 +0100

    Fixing paramaters

--- scripts/sql/db_operations/landing_to_refined.sql
+++ scripts/sql/db_operations/landing_to_refined.sql
@@ -16,13 +16,13 @@ WHERE
     STG.ID = TGT.ID
 ;
 
-DROP TEMPORARY TABLE IF EXISTS staging.staging_to_refined_testing_{{ region_name }}
+DROP TEMPORARY TABLE IF EXISTS staging.staging_to_refined_testing_{{ params.region_name }}
 ;
 
-CREATE TEMPORARY TABLE staging.staging_to_refined_new_{{ region_name }}
-SELECT staging.{{ region_name }}.* FROM staging.{{ region_name }}
-LEFT JOIN landing.{{ region_name }}{{ ds_nodash }}
-ON staging.{{ region_name }}.ID = landing.{{ region_name }}{{ ds_nodash }}.ID
+CREATE TEMPORARY TABLE staging.staging_to_refined_new_{{ params.region_name }}
+SELECT staging.{{ params.region_name }}.* FROM staging.{{ params.region_name }}
+LEFT JOIN landing.{{ params.region_name }}{{ ds_nodash }}
+ON staging.{{ params.region_name }}.ID = landing.{{ params.region_name }}{{ ds_nodash }}.ID
 ;
 
 INSERT INTO refined.ingested_for_sale_houses(ID, full_address, postcode, city, number_of_beds, links, description, price, first_seen, last_seen, seen_last_ingestion)
@@ -38,17 +38,15 @@ SELECT
     '{{ dag.timezone.convert(execution_date).strftime("%Y-%m-%d %H-%M-%S") }}',
     '{{ dag.timezone.convert(execution_date).strftime("%Y-%m-%d %H-%M-%S") }}',
     TRUE
-FROM staging.staging_to_refined_new_{{ region_name }} STG
+FROM staging.staging_to_refined_new_{{ params.region_name }} STG
 ;
 
 UPDATE
     refined.ingested_for_sale_houses TGT
-    staging.staging_to_refined_new_{{ region_name }} STG
+    staging.staging_to_refined_new_{{ params.region_name }} STG
 SET
     seen_last_ingestion = FALSE
 WHERE
     STG.ID != TGT.ID
 AND STG.city = TGT.city
 ;
-
-
