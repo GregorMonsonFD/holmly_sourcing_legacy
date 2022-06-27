c2a2770c2d5a5fb9c321d3ed9492afbd462122db
Gregor Monson <gregor.monson@fanduel.com>
2022-06-27 20:08:30 +0100

    Creating incremental listing area load and update

---
+++ scripts/sql/floorplan/incremental_new_records.sql
@@ -0,0 +1,21 @@
+DROP TABLE IF EXISTS  refined.area_export;
+CREATE TEMPORARY TABLE refined.area_export AS
+    (
+        SELECT
+            ID,
+            links,
+            number_of_floorplans,
+            area
+        FROM refined.ingested_for_sale_houses
+        WHERE number_of_floorplans is null
+    )
+;
+
+SELECT
+    *
+FROM refined.area_export
+INTO OUTFILE '/var/lib/mysql-files/area_export_{{ ds_nodash }}.csv'
+FIELDS OPTIONALLY ENCLOSED BY '"'
+TERMINATED BY ','
+ESCAPED BY ''
+LINES TERMINATED BY '\r\n';
