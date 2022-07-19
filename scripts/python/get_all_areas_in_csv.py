8bebcbfe4f1a848bb0ebce74665cd9c814f6dce1
Gregor Monson <gregor.monson@fanduel.com>
2022-07-19 17:10:09 +0100

    Testing csv extraction

--- scripts/python/get_all_areas_in_csv.py
+++ scripts/python/get_all_areas_in_csv.py
@@ -3,16 +3,15 @@ import pandas as pd
 
 from extract_area import get_floorplan_text
 
-def get_all_areas(ds ,**kwargs):
+def get_all_areas(ds):
     regex_ft = '\d+(?=\s*(?:sq|square)(?:\.\s*|\s*)f)'
     regex_m = '\d+(?=\s*(?:sq|square)(?:\.\s*|\s*)m)'
 
-    df = pd.read_csv()
+    df = pd.read_csv("tmp_data/area_export_{{ ds_nodash }}.csv")
 
     x = re.findall(regex_ft, placeholder_text)
     y = re.findall(regex_m, placeholder_text)
 
 
     print(x, y)
-
-get_all_areas()
+    print(df)
