d077e4b992aff023f80c996e5b16976e76043266
Gregor Monson <gregor.monson@fanduel.com>
2022-07-21 16:34:08 +0100

    Added row handler for csv import

--- scripts/python/extract_text.py
+++ scripts/python/extract_text.py
@@ -5,15 +5,19 @@ import requests
 from pytesseract import pytesseract
 
 
-def extract_text_from_link(rightmove_link: str):
-    floorplan_link = get_floorplan(rightmove_link)[0]
+def get_floorplan_text(rightmove_link: str):
+    floorplan_link = get_floorplan(rightmove_link)
+
+    if len(floorplan_link) == 0:
+        return 0
+
     config = ('-l eng --oem 1 --psm 3')
 
-    img_data = requests.get(floorplan_link).content
-    with open(f'floorplan.jpg', 'wb') as handler:
+    img_data = requests.get(floorplan_link[0]).content
+    with open('tmp_data/floorplan.jpg', 'wb') as handler:
         handler.write(img_data)
 
-    img = cv2.imread(f'floorplan.jpg')
+    img = cv2.imread('tmp_data/floorplan.jpg')
 
     img = cv2.resize(img, (0, 0), fx=2, fy=2)
 
@@ -21,4 +25,4 @@ def extract_text_from_link(rightmove_link: str):
 
     text = pytesseract.image_to_string(img, config=config)
 
-    return(text)
+    return [text, len(floorplan_link)]
--- scripts/python/get_all_areas_in_csv.py
+++ scripts/python/get_all_areas_in_csv.py
@@ -3,17 +3,42 @@
 import re
 import pandas as pd
 
-from extract_area import get_floorplan_text
+from extract_text import get_floorplan_text
 
 def get_all_areas(ds):
     regex_ft = '\d+(?=\s*(?:sq|square)(?:\.\s*|\s*)f)'
     regex_m = '\d+(?=\s*(?:sq|square)(?:\.\s*|\s*)m)'
 
-    df = pd.read_csv(f"/home/eggzo/airflow/tmp_data/area_export_{ ds }.csv", header=None)
+    sq_m_to_ft_factor = 10.7639
 
-    #x = re.findall(regex_ft, placeholder_text)
-    #y = re.findall(regex_m, placeholder_text)
+    input_df = pd.read_csv(f"/home/eggzo/airflow/tmp_data/area_export_{ ds }.csv", header=None)
+    output_df = pd.DataFrame()
 
+    for index, row in input_df.iterrows():
+        sq_ft_total = 0
+        text = get_floorplan_text(row[1])
 
-    #print(x, y)
-    print(df)
+        if text == 0:
+            output_df.append([row[0], row[1], 0, 'null', 'null'], ignore_index=True)
+
+        sq_ft = re.findall(regex_ft, text[0])
+        sq_m = re.findall(regex_m, text[0])
+
+        if len(sq_ft) != 0:
+            for area in sq_ft:
+                sq_ft_total = sq_ft_total + area
+
+                output_df.append([row[0], row[1], text[1], sq_ft_total, text], ignore_index=True)
+        elif len(sq_m) != 0:
+            for area in sq_m:
+                sq_ft_total = sq_ft_total + area
+
+            sq_ft_total = sq_ft_total * sq_m_to_ft_factor
+
+            output_df.append([row[0], row[1], text[1], sq_ft_total, text], ignore_index=True)
+
+        elif len(sq_m) == 0 and len(sq_ft) == 0:
+
+            output_df.append([row[0], row[1], text[1], 'null', text], ignore_index=True)
+
+    print(output_df)
