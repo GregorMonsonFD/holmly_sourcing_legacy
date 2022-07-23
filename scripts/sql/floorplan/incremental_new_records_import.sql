415140c0b3e2065aad265e8dcc2985db5d5e3461
Gregor Monson <gregor.monson@fanduel.com>
2022-07-23 17:15:27 +0100

    Testing new import clause

--- scripts/sql/floorplan/incremental_new_records_import.sql
+++ scripts/sql/floorplan/incremental_new_records_import.sql
@@ -4,7 +4,7 @@ CREATE TABLE landing.area_import_{{ ds_nodash }}
     ID                      bigint not null,
     links                   varchar(256),
     number_of_floorplans    smallint,
-    area                    varchar(10),
+    area                    varchar(128),
     raw_floorplan_output    varchar(2048)
 );
 
@@ -12,7 +12,7 @@ LOAD DATA INFILE '/var/lib/mysql-files/area_export_{{ ds_nodash }}_filled.csv'
 INTO TABLE landing.area_import_{{ ds_nodash }}
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
-LINES TERMINATED BY '"\n'
+LINES TERMINATED BY '\n'
 ;
 
 UPDATE      refined.ingested_for_sale_houses ifs
@@ -23,7 +23,7 @@ SET
     IF (
         ai.area = 'null',
         ifs.area = null,
-        ifs.area = CAST(ai.area AS SMALLINT)
+        ifs.area = CAST(ai.area AS FLOAT)
     )
 
     ifs.raw_floorplan_output  = ai.raw_floorplan_output
