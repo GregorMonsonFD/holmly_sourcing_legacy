9f1ba48837ab45f18feffff65639ba0b94dd9165
Gregor Monson <gregor.monson@fanduel.com>
2022-08-02 18:21:52 +0100

    Not using Nomatim, gmaps ftw

--- scripts/python/get_all_coordinates_in_csv.py
+++ scripts/python/get_all_coordinates_in_csv.py
@@ -1,23 +1,20 @@
 # from get_all_areas_in_csv import get_all_areas
 
-import re
-from geopy.geocoders import Nominatim
+from googlemaps import GoogleMaps
 import pandas as pd
-
-from scripts.python.extract_text import get_floorplan_text
 
 def get_all_coordinates(**kwargs):
     ds_nodash = kwargs['ds']
     input_df = pd.read_csv(f"/home/eggzo/airflow/tmp_data/coordinates_export_{ ds_nodash }.csv", header=None)
     output_df = pd.DataFrame(columns=['id', 'longitude', 'latitude'])
 
-    geolocator = Nominatim(user_agent="gm_finding_for_sale_house_coordinates")
+    gmaps = GoogleMaps({{conn.googlemapsapi.password}})
 
     for index, row in input_df.iterrows():
 
-        location = geolocator.geocode(row[1])
+        lat, lng = gmaps.address_to_latlng(row[1])
 
-        tmp_data = [row[0], location.longitude, location.latitude]
+        tmp_data = [row[0], lng, lat]
         print(tmp_data)
 
         output_df.loc[len(output_df)] = tmp_data
