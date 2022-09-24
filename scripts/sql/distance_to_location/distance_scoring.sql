ce5db854991f1b70856b0794e20272bb79055726
Gregor Monson <gregor.monson@fanduel.com>
2022-09-24 21:26:25 +0100

    Removing mysql variables

--- scripts/sql/distance_to_location/distance_scoring.sql
+++ scripts/sql/distance_to_location/distance_scoring.sql
@@ -1,21 +1,18 @@
 DROP TABLE IF EXISTS reporting.{{ params.location }};
 CREATE TABLE reporting.{{ params.location }} (
-    score                   double
-    , price_per_sq_ft       double
-    , distance_in_km        double
+    score                   double precision
+    , price_per_sq_ft       double precision
+    , distance_in_km        double precision
     , price                 decimal(11,2)
     , full_address          varchar(256)
     , number_of_beds        smallint
     , links                 varchar(256)
     , postcode              varchar(10)
-    , seen_last_ingestion   tinyint(1)
-    , area                  double
-    , longitude             double
-    , latitude              double
+    , seen_last_ingestion   boolean
+    , area                  double precision
+    , longitude             double precision
+    , latitude              double precision
 );
-
-SET @long = {{ params.long }};
-SET @lat = {{ params.lat }};
 
 DROP TABLE IF EXISTS staging.{{ params.location }}_analysis_staging;
 CREATE TEMPORARY TABLE staging.{{ params.location }}_analysis_staging AS
@@ -24,10 +21,10 @@ CREATE TEMPORARY TABLE staging.{{ params.location }}_analysis_staging AS
             (price/area) as price_per_sq_ft,
             (
                 111.111 *
-                DEGREES(ACOS(LEAST(1.0, COS(RADIANS(@lat))
+                DEGREES(ACOS(LEAST(1.0, COS(RADIANS({{ params.lat }}))
                  * COS(RADIANS(latitude))
-                 * COS(RADIANS(@long - longitude))
-                 + SIN(RADIANS(@lat))
+                 * COS(RADIANS({{ params.long }} - longitude))
+                 + SIN(RADIANS({{ params.lat }}))
                  * SIN(RADIANS(latitude)))))
             ) AS distance_in_km,
             price,
@@ -52,7 +49,7 @@ CREATE TEMPORARY TABLE staging.{{ params.location }}_analysis_final AS
                distance_in_km,
                price,
                full_address,
-               number_of_beds,
+               number_of_   qbeds,
                links,
                postcode,
                seen_last_ingestion,
