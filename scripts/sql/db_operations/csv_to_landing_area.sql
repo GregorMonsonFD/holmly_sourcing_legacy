8ad0e01c0c10e2982f5f3e9969b1162eee396554
Gregor Monson <gregormonson@gmail.com>
2022-09-29 21:18:51 +0100

    Changing null case

--- scripts/sql/db_operations/csv_to_landing_area.sql
+++ scripts/sql/db_operations/csv_to_landing_area.sql
@@ -2,7 +2,7 @@ COPY landing.{{ params.region_name }}{{ ds_nodash }}
 FROM '/tmp/rightmove_scrape/sales_data_{{ params.rightmove_region }}_{{ ds }}.csv'
 DELIMITER ','
 ESCAPE '"'
-NULL 'null'
+NULL 'NULL'
 CSV
 HEADER
 ;
