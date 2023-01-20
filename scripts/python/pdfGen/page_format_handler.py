6bce76edb02392f3f29c729cf61b1e3bcf1bf7d7
Gregor Monson <gregor.monson@fanduel.com>
2023-01-20 19:35:47 +0000

    Iterating on report gen

--- scripts/python/pdfGen/page_format_handler.py
+++ scripts/python/pdfGen/page_format_handler.py
@@ -4,33 +4,50 @@ from reportlab.pdfgen import canvas
 from reportlab.graphics.shapes import Line, LineShape, Drawing
 from reportlab.lib.pagesizes import LETTER, inch
 from reportlab.lib.enums import TA_LEFT, TA_RIGHT, TA_CENTER, TA_JUSTIFY
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+from datetime import datetime
+
+pdfmetrics.registerFont(TTFont('Poppins-Bold', './fonts/Poppins-Bold.ttf'))
+pdfmetrics.registerFont(TTFont('Poppins-Light', './fonts/Poppins-Light.ttf'))
+pdfmetrics.registerFont(TTFont('Poppins-Medium', './fonts/Poppins-Medium.ttf'))
+pdfmetrics.registerFont(TTFont('Poppins-Regular', './fonts/Poppins-Regular.ttf'))
+pdfmetrics.registerFont(TTFont('Poppins-SemiBold', './fonts/Poppins-SemiBold.ttf'))
 
 def front_page(elements):
-    img = Image('sps.png', kind='proportional')
-    img.drawHeight = 0.5 * inch
-    img.drawWidth = 2.4 * inch
-    img.hAlign = 'LEFT'
-    elements.append(img)
 
-    spacer = Spacer(30, 100)
-    elements.append(spacer)
+    title_style = ParagraphStyle('title', fontName='Poppins-Bold', fontSize=70, leading=72,
+                                 alignment=TA_LEFT, leftIndent=0)
 
-    img = Image('sps.png')
-    img.drawHeight = 2.5 * inch
-    img.drawWidth = 5.5 * inch
-    elements.append(img)
+    subtitle_style = ParagraphStyle('title', fontName='Poppins-SemiBold', fontSize=40, leading=72,
+                                 alignment=TA_LEFT, leftIndent=0)
 
-    spacer = Spacer(10, 250)
-    elements.append(spacer)
+    summary_style = ParagraphStyle('summary', fontName='Poppins-Light', fontSize=12, leading=20, justifyBreaks=1,
+                                   alignment=TA_LEFT, justifyLastLine=1)
 
-    psDetalle = ParagraphStyle('summary', fontSize=10, leading=14, justifyBreaks=1, alignment=TA_LEFT,
-                               justifyLastLine=1)
-    text = """Scottish Property Sourcing Test Report<br/>
+    title_text = 'Scottish Property Sourcing'
+
+    subtitle_text = 'Test Report'
+
+    summary_text = """
     Report Type: Testing Formatting<br/>
     Publication Date: 01-Jan-2023<br/>
     """
-    paragraphReportSummary = Paragraph(text, psDetalle)
-    elements.append(paragraphReportSummary)
+
+    title = Paragraph(title_text, title_style)
+    elements.append(title)
+
+    spacer = Spacer(10, 185)
+    elements.append(spacer)
+
+    subtitle = Paragraph(subtitle_text, subtitle_style)
+    elements.append(subtitle)
+
+    spacer = Spacer(10, 10)
+    elements.append(spacer)
+
+    paragraph_report_summary = Paragraph(summary_text, summary_style)
+    elements.append(paragraph_report_summary)
     elements.append(PageBreak())
 
     return elements
@@ -85,21 +102,32 @@ class page_format_handler(canvas.Canvas):
             self.__dict__.update(page)
             if (self._pageNumber > 1):
                 self.draw_canvas(page_count)
+            elif (self._pageNumber <= 1):
+                self.draw_front_page()
             canvas.Canvas.showPage(self)
         canvas.Canvas.save(self)
 
     def draw_canvas(self, page_count):
         page = "Page %s of %s" % (self._pageNumber, page_count)
+        copyright = f'© 2022 - {datetime.today().year} Holmly Ltd. All Rights Reserved'
         x = 128
         self.saveState()
         self.setStrokeColorRGB(0, 0, 0)
         self.setLineWidth(0.5)
-        self.drawImage("sps.png", self.width - inch * 8 - 5, self.height - 50, width=100, height=25,
-                       preserveAspectRatio=True)
-        self.drawImage("sps.png", self.width - inch * 2, self.height - 50, width=100, height=25,
+        self.setFont('Poppins-Bold', 16)
+        self.drawImage("sps_logo.png", self.width - inch * 9 + 30, self.height - 45, width=100, height=35,
                        preserveAspectRatio=True, mask='auto')
+        self.drawString(66, 755, "Scottish Property Sourcing")
         self.line(30, 740, LETTER[0] - 50, 740)
         self.line(66, 78, LETTER[0] - 66, 78)
-        self.setFont('Helvetica', 10)
+        self.setFont('Poppins-Light', 10)
         self.drawString(LETTER[0] - x, 65, page)
+        self.drawString(66, 65, copyright)
+        self.restoreState()
+
+    def draw_front_page(self):
+        self.saveState()
+        self.setFont('Poppins-Light', 10)
+        self.drawImage("sps_logo.png", inch * 4 - 20, -inch * 0.3, width=700, height=700,
+                       preserveAspectRatio=True, mask='auto')
         self.restoreState()
--- scripts/python/pdfGen/report_generator.py
+++ scripts/python/pdfGen/report_generator.py
@@ -1,8 +1,6 @@
 from reportlab.platypus import (SimpleDocTemplate, Paragraph, PageBreak, Image, Spacer, Table, TableStyle)
-from reportlab.lib.enums import TA_LEFT, TA_RIGHT, TA_CENTER, TA_JUSTIFY
 from reportlab.lib.styles import ParagraphStyle, getSampleStyleSheet
 from reportlab.lib.pagesizes import LETTER, inch
-from reportlab.graphics.shapes import Line, LineShape, Drawing
 from reportlab.lib.colors import Color
 from page_format_handler import page_format_handler, front_page, header
 from table_generation import remoteSessionTableMaker
@@ -18,7 +16,7 @@ class colour_handler():
             Color((r / 255), (g / 255), (b / 255), 1)
         )
 
-    def get_colour(self, index):
+    def gc(self, index):
         return(self.colour_theme[index])
 
 path = 'hello_world.pdf'
@@ -27,32 +25,24 @@ colours = colour_handler()
 elements = []
 
 # colors - Azul turkeza 367AB3
-#colorOhkaGreen0
-#colorOhkaGreen1
-#colorOhkaGreen2
-#colorOhkaGreenLineas
 
-colours.add_colour(45, 166, 153)
-colours.add_colour(182, 227, 166)
-colours.add_colour(140, 222, 192)
-colours.add_colour(50, 140, 140)
+colours.add_colour(45, 166, 153)    #colorOhkaGreen0
+colours.add_colour(182, 227, 166)   #colorOhkaGreen1
+colours.add_colour(140, 222, 192)   #colorOhkaGreen2
+colours.add_colour(50, 140, 140)    #colorOhkaGreenLineas
 
-#colorOhkaBlue0
-#colorOhkaBlue1
-
-colours.add_colour(54, 122, 179)
-colours.add_colour(122, 180, 225)
+colours.add_colour(54, 122, 179)    #colorOhkaBlue0
+colours.add_colour(122, 180, 225)   #colorOhkaBlue1
 
 elements = front_page(elements)
-elements = header(elements, True, colours.get_colour(0), colours.get_colour(3))
-#elements = remoteSessionTableMaker(elements, colours.get_colour(4), colours.get_colour(5), colours.get_colour(3))
-#nextPagesHeader(False, colours.get_colour(0), colours.get_colour(3))
-#elements = remoteSessionTableMaker(elements, colours.get_colour(4), colours.get_colour(5), colours.get_colour(3))
-#nextPagesHeader(False, colours.get_colour(0), colours.get_colour(3))
-#elements = remoteSessionTableMaker(elements, colours.get_colour(4), colours.get_colour(5), colours.get_colour(3))
-#nextPagesHeader(False, colours.get_colour(0), colours.get_colour(3))
-#elements = remoteSessionTableMaker(elements, colours.get_colour(4), colours.get_colour(5), colours.get_colour(3))
+elements = header(elements, True, colours.gc(0), colours.gc(3))
+#elements = remoteSessionTableMaker(elements, colours.gc(4), colours.gc(5), colours.gc(3))
+#nextPagesHeader(False, colours.gc(0), colours.gc(3))
+#elements = remoteSessionTableMaker(elements, colours.gc(4), colours.gc(5), colours.gc(3))
+#nextPagesHeader(False, colours.gc(0), colours.gc(3))
+#elements = remoteSessionTableMaker(elements, colours.gc(4), colours.gc(5), colours.gc(3))
+#nextPagesHeader(False, colours.gc(0), colours.gc(3))
+#elements = remoteSessionTableMaker(elements, colours.gc(4), colours.gc(5), colours.gc(3))
 # Build
-print(elements)
 doc = SimpleDocTemplate(path, pagesize=LETTER)
 doc.multiBuild(elements, canvasmaker=page_format_handler)
