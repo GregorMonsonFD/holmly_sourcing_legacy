beab438184f50c3c2b0711ac8043e374eded434a
Gregor Monson <gregor.monson@fanduel.com>
2023-02-01 20:28:16 +0000

    Adding report dag

---
+++ dags/report_generation/report_generation_all.py
@@ -0,0 +1,52 @@
+from airflow.models import DAG
+from airflow.operators.python_operator import PythonOperator
+from airflow.providers.sftp.operators.sftp import SFTPOperator
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+from airflow.utils.dates import days_ago
+from scripts.python.pdfGen.report_generator import report_generator
+import datetime, os, yaml
+
+args = {
+    'owner': 'Gregor Monson',
+    'start_date': days_ago(1),
+    'yesterday': days_ago(2), # make start date in the past
+}
+
+LOCAL_PATH = os.path.dirname(__file__)
+
+dag = DAG(
+        dag_id='find_listing_area',
+        default_args=args,
+        schedule_interval='0 10 * * *',
+        template_searchpath=['/home/eggzo/airflow/scripts/sql/report_content'],# make this workflow happen every day
+    )
+
+with dag:
+    report_content_export = PostgresOperator(
+        task_id='sql_report_content_export',
+        sql='report_content_export.sql.sql',
+        postgres_conn_id="holmly-postgresql",
+        retries=3,
+    )
+
+
+    sftp_download_from_db_report_content = SFTPOperator(
+        task_id="sftp_download_from_db_report_content",
+        ssh_conn_id="holmly_sftp",
+        local_filepath="/home/eggzo/airflow/tmp_data/report_content_{{ ds_nodash }}.csv",
+        remote_filepath="/tmp/report_content/report_content_{{ ds_nodash }}.csv",
+        operation="get",
+        create_intermediate_dirs=True,
+        retries=3,
+    )
+
+    generate_report = PythonOperator(
+        task_id='find_all_floorplans_incremental',
+        provide_context=True,
+        python_callable=get_all_areas,
+        op_kwargs={'ds': '{{ ds_nodash }}'},
+        execution_timeout=datetime.timedelta(hours=16),
+        retries=1,
+    )
+
+    report_content_export >> sftp_download_from_db_report_content >> generate_report
--- scripts/python/pdfGen/report_generator.py
+++ scripts/python/pdfGen/report_generator.py
@@ -1,34 +1,35 @@
 from reportlab.platypus import (SimpleDocTemplate, Paragraph, PageBreak, Image, Spacer, Table, TableStyle)
-from reportlab.lib.styles import ParagraphStyle, getSampleStyleSheet
 from reportlab.lib.pagesizes import LETTER, inch
 from page_format_handler import page_format_handler, colour_handler, front_page, information_page
-
 import pandas as pd
 
-path = 'hello_world.pdf'
-input_df = pd.read_csv('report_content_test.csv', header=None)
-styleSheet = getSampleStyleSheet()
-colours = colour_handler()
-elements = []
+def report_generator(**kwargs):
+    ds_nodash = kwargs['ds']
 
-# colors - Azul turkeza 367AB
-colours.add_colour('colorGreen0', 39, 198, 190, 1)
-colours.add_colour('colorTitleGreen0', 39, 198, 200, 0.8)
-colours.add_colour('colorGreen0Transparent', 39, 198, 190, 0.07)
+    path = f'holmly_daily_report_{ds_nodash}.pdf'
+    input_df = pd.read_csv(f'report_content_{ds_nodash}.csv', header=None)
 
-colours.add_colour('colorBlue0', 39, 119, 181, 1)
-colours.add_colour('colorBlue0Transparent', 39, 119, 181, 0.07)
+    colours = colour_handler()
+    elements = []
 
-colours.add_colour('transparent', 255, 255, 255, 1)
+    # colors - Azul turkeza 367AB
+    colours.add_colour('colorGreen0', 39, 198, 190, 1)
+    colours.add_colour('colorTitleGreen0', 39, 198, 200, 0.8)
+    colours.add_colour('colorGreen0Transparent', 39, 198, 190, 0.07)
 
-elements = front_page(elements)
+    colours.add_colour('colorBlue0', 39, 119, 181, 1)
+    colours.add_colour('colorBlue0Transparent', 39, 119, 181, 0.07)
 
-summary = """
-        Top properties over 7% yield
-    """
+    colours.add_colour('transparent', 255, 255, 255, 1)
 
-elements = information_page(elements, colours, "Top Yield Properties", summary, input_df)
+    elements = front_page(elements)
 
-# Build
-doc = SimpleDocTemplate(path, pagesize=LETTER)
-doc.multiBuild(elements, canvasmaker=page_format_handler)
+    summary = """
+            Top properties over 7% yield
+        """
+
+    elements = information_page(elements, colours, "Top Yield Properties", summary, input_df)
+
+    # Build
+    doc = SimpleDocTemplate(path, pagesize=LETTER)
+    doc.multiBuild(elements, canvasmaker=page_format_handler)
